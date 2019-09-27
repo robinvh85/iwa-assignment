778277f44075ec747982385d7571092104e8a018
robinvh85 <robinvh85@gmail.com>
2019-09-27 15:33:26 +0700

    Add user management  - Add CRUD for user management  - Add scss for UI  - Add check active item for top_menu  - Custom validate email and password for user (not use validable of Devise)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def menu_active(menu_name)
+    'active' if controller_name == menu_name
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable, :registerable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable, :registerable, :validatable
   devise :database_authenticatable,
-         :rememberable, :validatable
+         :rememberable
 
-  enum role: [:teacher, :student]
+  enum role: [:Teacher, :Student]
+
+  validates :name, presence: true
+
+  validates :email, 
+    presence: true, 
+    uniqueness: true,
+    format: { with: Devise.email_regexp, allow_blank: true }
+  
+  # Validate password
+  validates :password, presence: true, on: :create
+  validates_length_of :password, within: Devise.password_length, allow_blank: true
+  validates :password, confirmation: true, unless: -> { password.blank? }
 end
