38e4e5a05f8db0bbbc393207a99117cb69da6a7a
robinvh85 <robinvh85@gmail.com>
2019-09-29 08:28:25 +0700

    Improve: check role when login  - Add custom strategy for check role login  - Remove home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     sessions: 'sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'users#index'
 
   resources :users
 end
